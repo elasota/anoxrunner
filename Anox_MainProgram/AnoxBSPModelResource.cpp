808d0b8a5f926a45a703fa904867d5031d09ff35
elasota <1137273+elasota@users.noreply.github.com>
2026-01-05 18:32:27 -0500

    Refactor BSP loader to use single-file loader

--- Anox_MainProgram/AnoxBSPModelResource.cpp
+++ Anox_MainProgram/AnoxBSPModelResource.cpp
@@ -1,7 +1,6 @@
 #include "AnoxBSPModelResource.h"
 #include "AnoxMaterialResource.h"
-#include "AnoxLoadEntireFileJob.h"
-
+#include "AnoxAbstractSingleFileResource.h"
 #include "AnoxGameFileSystem.h"
 
 #include "anox/Data/BSPModel.h"
@@ -23,30 +22,24 @@ namespace anox
 	{
 	};
 
-	class AnoxBSPModelResourceLoader final : public AnoxBSPModelResourceLoaderBase
+	struct AnoxBSPModelResourceLoaderState final : public AnoxAbstractSingleFileResourceLoaderState
 	{
-	public:
-		rkit::Result CreateLoadJob(const rkit::RCPtr<AnoxBSPModelResourceBase> &resource, const AnoxResourceLoaderSystems &systems, const rkit::CIPathView &key, rkit::RCPtr<rkit::Job> &outJob) const override;
-		rkit::Result CreateResourceObject(rkit::UniquePtr<AnoxBSPModelResourceBase> &outResource) const override;
-	};
-
-	struct AnoxBSPModelResourceLoaderState final : public rkit::RefCounted
-	{
-		rkit::Vector<uint8_t> m_bspFileContents;
 		data::BSPDataChunks m_chunks;
 	};
 
-	class AnoxBSPLoaderAnalyzeJob final : public rkit::IJobRunner
+	struct AnoxBSPModelLoaderInfo
 	{
-	public:
-		explicit AnoxBSPLoaderAnalyzeJob(const rkit::RCPtr<AnoxBSPModelResource> &resource,
-			const rkit::RCPtr<AnoxBSPModelResourceLoaderState> &state, rkit::IJobQueue &jobQueue,
-			AnoxResourceManagerBase &resManager,
-			const rkit::RCPtr<rkit::JobSignaler> &waitForDependenciesSignaler);
+		typedef AnoxBSPModelResourceLoaderBase LoaderBase_t;
+		typedef AnoxBSPModelResource Resource_t;
+		typedef AnoxBSPModelResourceLoaderState State_t;
 
-		rkit::Result Run() override;
+		static constexpr bool kHasDependencies = true;
+		static constexpr bool kHasAnalysisPhase = true;
+		static constexpr bool kHasLoadPhase = true;
 
-	private:
+		static rkit::Result AnalyzeFile(State_t &state, Resource_t &resource, rkit::traits::TraitRef<rkit::VectorTrait<rkit::RCPtr<rkit::Job>>> outDeps);
+		static rkit::Result LoadFile(State_t &state, Resource_t &resource);
+
 		class ChunkReader
 		{
 		public:
@@ -58,71 +51,11 @@ namespace anox
 		private:
 			rkit::IReadStream &m_readStream;
 		};
-
-		rkit::RCPtr<AnoxBSPModelResource> m_resource;
-		rkit::RCPtr<AnoxBSPModelResourceLoaderState> m_state;
-		rkit::IJobQueue &m_jobQueue;
-		AnoxResourceManagerBase &m_resManager;
-		rkit::RCPtr<rkit::JobSignaler> m_waitForDependenciesSignaler;
 	};
 
-	class AnoxBSPLoaderProcessJob final : public rkit::IJobRunner
+	rkit::Result AnoxBSPModelLoaderInfo::AnalyzeFile(State_t &state, Resource_t &resource, rkit::traits::TraitRef<rkit::VectorTrait<rkit::RCPtr<rkit::Job>>> outDeps)
 	{
-	public:
-		explicit AnoxBSPLoaderProcessJob(const rkit::RCPtr<AnoxBSPModelResource> &resource,
-			const rkit::RCPtr<AnoxBSPModelResourceLoaderState> &state);
-
-		rkit::Result Run() override;
-
-	private:
-	};
-
-	rkit::Result AnoxBSPModelResourceLoader::CreateLoadJob(const rkit::RCPtr<AnoxBSPModelResourceBase> &resourceBase, const AnoxResourceLoaderSystems &systems, const rkit::CIPathView &key, rkit::RCPtr<rkit::Job> &outJob) const
-	{
-		// Jobs:
-		// - Load file job
-		// - Analysis job (queues dependency jobs)
-		// - Wait for dependencies job (signalled by join job created by analysis job)
-		// - Process job
-		rkit::RCPtr<AnoxBSPModelResource> resource = resourceBase.StaticCast<AnoxBSPModelResource>();
-
-		AnoxGameFileSystemBase &fileSystem = *systems.m_fileSystem;
-		rkit::IJobQueue &jobQueue = fileSystem.GetJobQueue();
-
-		rkit::RCPtr<AnoxBSPModelResourceLoaderState> loaderState;
-		RKIT_CHECK(rkit::New<AnoxBSPModelResourceLoaderState>(loaderState));
-
-		rkit::RCPtr<rkit::Job> waitForDependenciesJob;
-		rkit::RCPtr<rkit::JobSignaler> waitForDependenciesSignaler;
-		RKIT_CHECK(jobQueue.CreateSignaledJob(waitForDependenciesSignaler, waitForDependenciesJob));
-
-		rkit::RCPtr<rkit::Job> loadFileJob;
-		RKIT_CHECK(CreateLoadEntireFileJob(loadFileJob, loaderState.FieldRef(&AnoxBSPModelResourceLoaderState::m_bspFileContents), fileSystem, key));
-
-		rkit::UniquePtr<rkit::IJobRunner> analysisJobRunner;
-		RKIT_CHECK(rkit::New<AnoxBSPLoaderAnalyzeJob>(analysisJobRunner, resource, loaderState, jobQueue, *systems.m_resManager, waitForDependenciesSignaler));
-
-		rkit::RCPtr<rkit::Job> analysisJob;
-		RKIT_CHECK(fileSystem.GetJobQueue().CreateJob(nullptr, rkit::JobType::kNormalPriority, std::move(analysisJobRunner), loadFileJob));
-
-		outJob = std::move(waitForDependenciesJob);
-
-		return rkit::ResultCode::kOK;
-	}
-
-	AnoxBSPLoaderAnalyzeJob::AnoxBSPLoaderAnalyzeJob(const rkit::RCPtr<AnoxBSPModelResource> &resource,
-		const rkit::RCPtr<AnoxBSPModelResourceLoaderState> &state, rkit::IJobQueue &jobQueue, AnoxResourceManagerBase &resManager, const rkit::RCPtr<rkit::JobSignaler> &waitForDependenciesSignaler)
-		: m_resource(resource)
-		, m_state(state)
-		, m_jobQueue(jobQueue)
-		, m_resManager(resManager)
-		, m_waitForDependenciesSignaler(waitForDependenciesSignaler)
-	{
-	}
-
-	rkit::Result AnoxBSPLoaderAnalyzeJob::Run()
-	{
-		rkit::ReadOnlyMemoryStream stream(m_state->m_bspFileContents.GetBuffer(), m_state->m_bspFileContents.Count());
+		rkit::ReadOnlyMemoryStream stream(state.m_fileContents.GetBuffer(), state.m_fileContents.Count());
 
 		data::BSPFile bspFile;
 		RKIT_CHECK(stream.ReadAll(&bspFile, sizeof(bspFile)));
@@ -131,57 +64,56 @@ namespace anox
 			|| bspFile.m_version.Get() != data::BSPFile::kVersion)
 			return rkit::ResultCode::kDataError;
 
-		RKIT_CHECK(m_state->m_chunks.VisitAllChunks(ChunkReader(stream)));
+		RKIT_CHECK(state.m_chunks.VisitAllChunks(ChunkReader(stream)));
 
-		const data::BSPDataChunks &chunks = m_state->m_chunks;
+		const data::BSPDataChunks &chunks = state.m_chunks;
+		anox::AnoxResourceManagerBase &resManager = *state.m_systems.m_resManager;
 
-		size_t numDepJobs = 0;
-
-		rkit::Vector<rkit::RCPtr<rkit::Job>> resourceJobs;
+		// No SafeAdd since we don't really care about overflow here
+		RKIT_CHECK(outDeps.Reserve(chunks.m_materials.Count() + chunks.m_lightmaps.Count() + chunks.m_entityDefs.Count()));
 
 		for (const rkit::data::ContentID &materialContentID : chunks.m_materials)
 		{
 			rkit::RCPtr<rkit::Job> job;
 			rkit::Future<AnoxResourceRetrieveResult> result;
-			RKIT_CHECK(m_resManager.GetContentIDKeyedResource(&job, result, resloaders::kWorldMaterialTypeCode, materialContentID));
+			RKIT_CHECK(resManager.GetContentIDKeyedResource(&job, result, resloaders::kWorldMaterialTypeCode, materialContentID));
 
-			RKIT_CHECK(resourceJobs.Append(job));
+			RKIT_CHECK(outDeps.Append(job));
 		}
 
 		for (const rkit::data::ContentID &lightmapContentID : chunks.m_lightmaps)
 		{
 			rkit::RCPtr<rkit::Job> job;
 			rkit::Future<AnoxResourceRetrieveResult> result;
-			RKIT_CHECK(m_resManager.GetContentIDKeyedResource(&job, result, resloaders::kTextureResourceTypeCode, lightmapContentID));
+			RKIT_CHECK(resManager.GetContentIDKeyedResource(&job, result, resloaders::kTextureResourceTypeCode, lightmapContentID));
 
-			RKIT_CHECK(resourceJobs.Append(job));
+			RKIT_CHECK(outDeps.Append(job));
 		}
 
 		for (const rkit::data::ContentID &edefContentID : chunks.m_entityDefs)
 		{
 			rkit::RCPtr<rkit::Job> job;
 			rkit::Future<AnoxResourceRetrieveResult> result;
-			RKIT_CHECK(m_resManager.GetContentIDKeyedResource(&job, result, resloaders::kEntityDefTypeCode, edefContentID));
+			RKIT_CHECK(resManager.GetContentIDKeyedResource(&job, result, resloaders::kEntityDefTypeCode, edefContentID));
 
-			RKIT_CHECK(resourceJobs.Append(job));
-		}
-
-		{
-			rkit::UniquePtr<rkit::IJobRunner> dependenciesDoneJobRunner;
-			RKIT_CHECK(m_jobQueue.CreateSignalJobRunner(dependenciesDoneJobRunner, m_waitForDependenciesSignaler));
-			RKIT_CHECK(m_jobQueue.CreateJob(nullptr, rkit::JobType::kNormalPriority, std::move(dependenciesDoneJobRunner), resourceJobs.ToSpan().ToValueISpan()));
+			RKIT_CHECK(outDeps.Append(job));
 		}
 
 		return rkit::ResultCode::kOK;
 	}
 
-	AnoxBSPLoaderAnalyzeJob::ChunkReader::ChunkReader(rkit::IReadStream &readStream)
+	rkit::Result AnoxBSPModelLoaderInfo::LoadFile(State_t &state, Resource_t &resource)
+	{
+		return rkit::ResultCode::kNotYetImplemented;
+	}
+
+	AnoxBSPModelLoaderInfo::ChunkReader::ChunkReader(rkit::IReadStream &readStream)
 		: m_readStream(readStream)
 	{
 	}
 
 	template<class T>
-	rkit::Result AnoxBSPLoaderAnalyzeJob::ChunkReader::VisitMember(rkit::Vector<T> &vector) const
+	rkit::Result AnoxBSPModelLoaderInfo::ChunkReader::VisitMember(rkit::Vector<T> &vector) const
 	{
 		rkit::endian::LittleUInt32_t countData;
 		RKIT_CHECK(m_readStream.ReadAll(&countData, sizeof(countData)));
@@ -196,17 +128,14 @@ namespace anox
 		return rkit::ResultCode::kOK;
 	}
 
-	rkit::Result AnoxBSPModelResourceLoader::CreateResourceObject(rkit::UniquePtr<AnoxBSPModelResourceBase> &outResource) const
+	rkit::Result AnoxBSPModelResourceLoaderBase::Create(rkit::RCPtr<AnoxBSPModelResourceLoaderBase> &outLoader)
 	{
-		return rkit::New<AnoxBSPModelResource>(outResource);
-	}
+		typedef AnoxAbstractSingleFileResourceLoader<AnoxBSPModelLoaderInfo> Loader_t;
 
-	rkit::Result AnoxBSPModelResourceLoaderBase::Create(rkit::RCPtr<AnoxBSPModelResourceLoaderBase> &outFactory)
-	{
-		rkit::RCPtr<AnoxBSPModelResourceLoader> factory;
-		RKIT_CHECK(rkit::New<AnoxBSPModelResourceLoader>(factory));
+		rkit::RCPtr<Loader_t> loader;
+		RKIT_CHECK(rkit::New<Loader_t>(loader));
 
-		outFactory = std::move(factory);
+		outLoader = std::move(loader);
 
 		return rkit::ResultCode::kOK;
 	}
